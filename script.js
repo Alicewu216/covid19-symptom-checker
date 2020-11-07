be58e3f34bafd0c1398ce7b8247be815ed7e0d30
TJ Hutchinson <tjhutch92@gmail.com>
2020-11-07 13:28:59 -0800

    script.js update

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ $(document).ready(function(){
 
     baseURL ="https://endlessmedicalapi1.p.rapidapi.com/";
 
-    var initSessionID = baseURL + "/InitSession";
+    var initSessionID = baseURL + "InitSession";
     
+    topDiseasesResult = [];
+    variableImportancesResult = [];
+
     function getFeatures() {
         const settings = {
             "async": true,
             "crossDomain": true,
-            "url": "https://endlessmedicalapi1.p.rapidapi.com/GetFeatures",
+            "url": baseURL + "GetFeatures",
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": "b5e515e4efmshf7f7cf9142dd41ep17f3ebjsn9d08a632e406",
@@ -16,7 +19,7 @@ $(document).ready(function(){
             }
         };
         
-        $.ajax(settings).then(function (result) {
+        $.ajax(settings).then(function (response) {
             console.log(response);
         });
     }
@@ -25,7 +28,7 @@ $(document).ready(function(){
         const settings = {
             "async": true,
             "crossDomain": true,
-            "url": "https://endlessmedicalapi1.p.rapidapi.com/GetOutcomes",
+            "url": baseURL + "GetOutcomes",
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": "b5e515e4efmshf7f7cf9142dd41ep17f3ebjsn9d08a632e406",
@@ -38,34 +41,111 @@ $(document).ready(function(){
         });
     }
 
-    function analyze(sessionID) {
-        var analyze = baseURL + "/Analyze?SessionID=" + sessionID;
-        console.log(analyze);
+    var sessionID = null; // SessionID
+
+    function InitializeSessionIfNecessary()
+    {
+        if (null != sessionID)
+            return;
+
+
+            $.ajax({
+                async: true,
+                crossDomain: true,
+                url: initSessionID,
+                method: "GET",
+                headers: {
+                    "x-rapidapi-key": "b5e515e4efmshf7f7cf9142dd41ep17f3ebjsn9d08a632e406",
+                    "x-rapidapi-host": "endlessmedicalapi1.p.rapidapi.com"
+                }
+            }).then(function(response){
+                if (response.status == 'ok'){
+                //console.log(response);
+                var sessionID = response.SessionID;
+                console.log(sessionID);
+        
+                //termsOfAgreement(sessionID);
+                analyze(sessionID);
+                getOutcomes();
+                getFeatures();
+                }
+            });
+        
     }
-
-    $.ajax({
-        async: true,
-        crossDomain: true,
-        url: initSessionID,
-        method: "GET",
-        headers: {
-            "x-rapidapi-key": "b5e515e4efmshf7f7cf9142dd41ep17f3ebjsn9d08a632e406",
-            "x-rapidapi-host": "endlessmedicalapi1.p.rapidapi.com"
-        }
-    }).then(function(response){
-        //console.log(response);
-        var sessionID = response.SessionID;
-        console.log(sessionID);
-
-        //termsOfAgreement(sessionID);
-        analyze(sessionID);
-        getOutcomes();
-        getFeatures();
-    });
-
-
+    
+    InitializeSessionIfNecessary();
 
     
+    function updateFeature(symptom){
+        const settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://endlessmedicalapi1.p.rapidapi.com/UpdateFeature?SessionID=" + sessionID + "&name=" + symptom.name + "&value=" + symptom.value,
+            "method": "POST",
+            "headers": {
+                "x-rapidapi-key": "b5e515e4efmshf7f7cf9142dd41ep17f3ebjsn9d08a632e406",
+                "x-rapidapi-host": "endlessmedicalapi1.p.rapidapi.com"
+            }
+        };
+        
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            if (result.data.status == 'ok') {
+                setTimeout( DisplaySuggestedTests(), 1000 ); // 10.08.2019
+            }
+            else if (result.data.status == 'error') {
+
+            }
+        });
+    }
+
+    function DeleteSymptom(symptom)
+    {
+        const settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://endlessmedicalapi1.p.rapidapi.com/DeleteFeature",
+            "method": "POST",
+            "headers": {
+                "x-rapidapi-key": "b5e515e4efmshf7f7cf9142dd41ep17f3ebjsn9d08a632e406",
+                "x-rapidapi-host": "endlessmedicalapi1.p.rapidapi.com"
+            }
+        };
+        
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+    }
+
+    function analyze(symptom) {
+        var analyze = baseURL + "/Analyze?SessionID=" + sessionID;
+        const settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": analyze,
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "b5e515e4efmshf7f7cf9142dd41ep17f3ebjsn9d08a632e406",
+                "x-rapidapi-host": "endlessmedicalapi1.p.rapidapi.com"
+            }
+        };
+        
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            if (result.data.status == 'ok') {
+                self.TopDiseasesResult = result.data.Diseases;
+                self.VariableImportancesResult = result.data.VariableImportances;
+            }
+            else if (result.data.status == 'error') {
+
+            }
+
+            document.getElementById('ifDetails').contentDocument.body.innerHTML = JSON.stringify( result.data );
+        });
+    }
+    
+
+    /*
     function termsOfAgreement(sessionID) {
         var acceptTerms = baseURL + "AcceptTermsOfUse?SessionID=" + sessionID + "&passphrase=I%20have%20read%2C%20understood%20and%20I%20accept%20and%20agree%20to%20comply%20with%20the%20Terms%20of%20Use%20of%20EndlessMedicalAPI%20and%20Endless%20Medical%20services.%20The%20Terms%20of%20Use%20are%20available%20on%20endlessmedical.com"
         console.log(acceptTerms);
@@ -79,24 +159,13 @@ $(document).ready(function(){
             }
         })
     };
-    
-    function updateFeature(sessionID){
-        const settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://endlessmedicalapi1.p.rapidapi.com/UpdateFeature?SessionID=" + sessionID+ "&name=" + name + "&value=" + age,
-            "method": "POST",
-            "headers": {
-                "x-rapidapi-key": "b5e515e4efmshf7f7cf9142dd41ep17f3ebjsn9d08a632e406",
-                "x-rapidapi-host": "endlessmedicalapi1.p.rapidapi.com"
-            }
-        };
-        
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-        });
-    }
+    */
+
+
 
     });
 
 
+
+
+    /* Age, Chills, HistoryFever, */
